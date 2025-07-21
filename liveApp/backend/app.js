ba76941df00efeac3b0a9faf6fa18da24f69e701
Tadiwanashe Songore <119779084+TADI-I@users.noreply.github.com>
2025-07-21 09:06:22 +0200

    App.js
    Apple
    .

--- liveApp/backend/app.js
+++ liveApp/backend/app.js
@@ -8,6 +8,7 @@ const sequelize = require('./config/database');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Import models (this ensures associations are loaded if needed)
 require('./models/User');
